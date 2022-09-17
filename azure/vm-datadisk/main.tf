8bc22a7b199a5c7b44bc646d1b9c2197f283c991
blastomussa <jccourtn@gmail.com>
2022-09-16 23:25:06 -0400

    update

--- azure/vm-datadisk/main.tf
+++ azure/vm-datadisk/main.tf
@@ -102,8 +102,6 @@ resource "azurerm_linux_virtual_machine" "my_terraform_vm" {
     storage_account_type = "Premium_LRS"
   }
 
-  
-
   source_image_reference {
     publisher = "Canonical"
     offer     = "UbuntuServer"
